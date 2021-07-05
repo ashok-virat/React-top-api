1b9935863908a54e3e0533cb065e8e7494ea0566
sheik <ashok@tmmmt.sa>
2021-07-05 08:07:50 +0530

    usecallback added

--- src/Component/UseMemo/UseMemoChild.js
+++ src/Component/UseMemo/UseMemoChild.js
@@ -1,11 +1,16 @@
 import React, { memo } from "react";
 
 const UseMemoChild = memo((props) => {
-  const { message } = props;
+  const { decreaseCount } = props;
   //memo component only render if the component props changes
   //see the console
   console.log("use memo child render");
-  return <div>{message}</div>;
+  return (
+    <div>
+      <h3>ChildComponent</h3>
+      <button onClick={decreaseCount}>decreaseParentCount</button>
+    </div>
+  );
 });
 
 export default UseMemoChild;
--- src/Component/UseMemo/UseMemoParent.js
+++ src/Component/UseMemo/UseMemoParent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import UseMemoChild from "./UseMemoChild";
 
 const useMemoParent = () => {
@@ -12,11 +12,16 @@ const useMemoParent = () => {
     setCount({ ...count, count: count.count + 1 });
   };
 
+  //This is useful when passing callbacks to optimized child components that rely on reference equality to prevent unnecessary renders
+  const decreaseCount = useCallback(() => {
+    setCount((count) => ({ count: count.count - 1 }));
+  }, []);
+
   return (
     <div>
       <h1>Count:{count.count}</h1>
       <button onClick={increaseCount}>InCrease Count</button>
-      <UseMemoChild message={count.message}></UseMemoChild>
+      <UseMemoChild decreaseCount={decreaseCount}></UseMemoChild>
     </div>
   );
 };
