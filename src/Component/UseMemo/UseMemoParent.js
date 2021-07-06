9c2bbcb14e9ba0a917c4f0c6675ef6be8aead5d2
sheik <ashok@tmmmt.sa>
2021-07-06 07:47:12 +0530

    count added

--- src/Component/UseMemo/UseMemoParent.js
+++ src/Component/UseMemo/UseMemoParent.js
@@ -12,24 +12,30 @@ const useMemoParent = () => {
     setCount({
       ...count,
       count: count.count + 1,
-      message: count.count === 3 ? "see the console _use memo called_" : count.message,
+      message:
+        count.count === 3 ? "see the console _use memo called_" : "hi ashok",
     });
   };
 
   //This is useful when passing callbacks to optimized child components that rely on reference equality to prevent unnecessary renders
   const decreaseCount = useCallback(() => {
-    setCount((count) => ({ count: count.count - 1 }));
+    setCount((count) => ({
+      count: count.count - 1,
+      message:
+        count.count === 3 ? "see the console _use memo called_" : "hi ashok",
+    }));
   }, []);
 
   //useCallback returns its function uncalled so you can call it later, while useMemo calls its function and returns the result.
   const getCount = useMemo(() => {
     console.log("usememo called");
-    return <div>{count.message}</div>;
+    return <p>{count.message}</p>;
   }, [count.message]);
 
   return (
     <div>
-      <h1>Count:{getCount}</h1>
+      <h1>Count:{count.count}</h1>
+      <h3 style={{ color: "red" }}>{getCount}</h3>
       <button onClick={increaseCount}>InCrease Count</button>
       <UseMemoChild decreaseCount={decreaseCount}></UseMemoChild>
     </div>
